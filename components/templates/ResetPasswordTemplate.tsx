41307e6fb5526867d9b1e3c7dd92cc545bd30948
Truong <truongtdph40209@fpt.edu.vn>
2024-10-07 23:09:33 +0700

    Fix lint

--- components/templates/ResetPasswordTemplate.tsx
+++ components/templates/ResetPasswordTemplate.tsx
@@ -1,73 +1,60 @@
-import { useRouter } from "expo-router";
-import { map } from "lodash";
-import React from "react";
-import { StyleSheet, useColorScheme } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView, View } from "tamagui";
+import { LockKeyhole } from '@tamagui/lucide-icons'
+import React from 'react'
+import { StyleSheet, useColorScheme } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { View } from 'tamagui'
 
-import ContentTitle from "~/components/atoms/ContentTitle";
-import InputForm from "~/components/molecules/InputForm";
-import LinearGradientBackground from "~/components/molecules/LinearGradientBackground";
-import TextWithLink from "~/components/molecules/TextWithLink";
-import useTranslation from "~/hooks/useTranslation";
-import InputWithIcons from "../atoms/InputWithIcons";
-import { LockKeyhole } from "@tamagui/lucide-icons";
-import getColors from "~/constants/Colors";
-import { PositiveButton } from '~/components/atoms/PositiveButton';
+import ContentTitle from '~/components/atoms/ContentTitle'
+import InputWithIcons from '~/components/atoms/InputWithIcons'
+import { PositiveButton } from '~/components/atoms/PositiveButton'
+import LinearGradientBackground from '~/components/molecules/LinearGradientBackground'
+import getColors from '~/constants/Colors'
+import useTranslation from '~/hooks/useTranslation'
 
 const ResetPasswordTemplate: React.FC = (): JSX.Element => {
-  const { t } = useTranslation();
-  const router = useRouter();
-  const colors = getColors(useColorScheme());
-
-  const handleSignUp = (): void => {
-    router.replace("/(tabs)/home");
-  };
-
-  const redirectToLogin = (): void => {
-    router.back();
-  };
+  const { t } = useTranslation()
+  const colors = getColors(useColorScheme())
 
   return (
     <LinearGradientBackground>
-        <SafeAreaView style={styles.container}>
-          <View marginTop={"13%"}>
-            <ContentTitle
-              title={t("screens.resetPassword.newPassword")}
-              subtitle={t("screens.resetPassword.resetPasswordPrompt")}
-            />
-          </View>
+      <SafeAreaView style={styles.container}>
+        <View marginTop={'13%'}>
+          <ContentTitle
+            title={t('screens.resetPassword.newPassword')}
+            subtitle={t('screens.resetPassword.resetPasswordPrompt')}
+          />
+        </View>
 
-          <View marginTop={"25%"} gap={20}>
-            <InputWithIcons
-              iconRight={<LockKeyhole size={16} color={colors.oceanTeal} />}
-              placeholder={t("screens.resetPassword.newPassword")}
-              onChangeText={() => {}}
-            />
+        <View marginTop={'25%'} gap={20}>
+          <InputWithIcons
+            iconRight={<LockKeyhole size={16} color={colors.oceanTeal} />}
+            placeholder={t('screens.resetPassword.newPassword')}
+            onChangeText={() => {}}
+          />
 
-            <InputWithIcons
-              iconRight={<LockKeyhole size={16} color={colors.oceanTeal} />}
-              placeholder={t("screens.resetPassword.confirmNewPassword")}
-              onChangeText={() => {}}
-            />
-          </View>
+          <InputWithIcons
+            iconRight={<LockKeyhole size={16} color={colors.oceanTeal} />}
+            placeholder={t('screens.resetPassword.confirmNewPassword')}
+            onChangeText={() => {}}
+          />
+        </View>
 
-          <View flex={1} justifyContent="flex-end">
-            <PositiveButton
-                title={t("screens.resetPassword.confirmNewPassword")}
-            />
-          </View>
-        </SafeAreaView>
- 
+        <View flex={1} justifyContent="flex-end">
+          <PositiveButton
+            title={t('screens.resetPassword.confirmNewPassword')}
+          />
+        </View>
+      </SafeAreaView>
+
     </LinearGradientBackground>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-  },
-});
+    padding: 20
+  }
+})
 
-export default ResetPasswordTemplate;
+export default ResetPasswordTemplate
